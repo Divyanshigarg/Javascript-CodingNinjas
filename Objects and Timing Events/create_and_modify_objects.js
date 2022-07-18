fa702cbdfc163446d9857b368839499edeca0f97
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-07-18 10:16:17 +0530

    create the objects

---
+++ Objects and Timing Events/create_and_modify_objects.js
@@ -0,0 +1,13 @@
+var student = {
+    name:'Divi',
+    age:21,
+    rollno:12,
+    
+    address : {
+       city : 'Belgaum',
+       state : 'New Delhi'
+  }
+};
+student
+delete student.rollno
+student
