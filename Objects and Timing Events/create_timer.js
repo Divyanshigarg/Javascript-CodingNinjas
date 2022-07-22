efe30c8db6282b2041a5bf30f362981166e31656
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-07-22 21:03:50 +0530

    create timer

---
+++ Objects and Timing Events/create_timer.js
@@ -0,0 +1,10 @@
+function timer() {
+    for(let i = 1; i<=5; i++){
+        setTimeout(function() {
+           //Write your code here...
+          console.log(i)
+        }, 10);//write your code here...);
+    }
+    console.log("Hello ninjas!");
+}
+timer();
