20f5369ffde551226d16a220a16de10d1ccda09d
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-08-20 18:06:59 +0530

    callback-function

--- Callback_functions/callback_function.js
+++ Callback_functions/callback_function.js
@@ -1,1 +1,22 @@
-callback_function.js
+
+//what is callback function
+if you call a function and pass a function inside another function.
+This function is called as callback function.
+They are very powerful in js .
+They gives as the access to the whole asyncronous world in a single synchronous threaded language.
+
+//formal defination
+A callback function is a function passed into another function as an argument,
+ which is then invoked inside the outer function to complete some kind of routine or action.
+
+ //eg
+ function x() {
+    
+ }
+ x(function y()
+ {
+
+ })
+
+ //here the function y is called as a callback function
+
