fb98d284fef58f1dcc9a36e70a6e205288ebd5f6
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-08-23 16:55:02 +0530

    about-confirm-box

---
+++ Dialogue Boxes/confirm_box/confirm_box.js
@@ -0,0 +1,3 @@
+A confirm box is often used if you want the user to verify or accept something. 
+When a confirm box pops up, the user will have to click either "OK" or "Cancel" to proceed. 
+If the user clicks "OK", the box returns true. If the user clicks "Cancel", the box returns false.
