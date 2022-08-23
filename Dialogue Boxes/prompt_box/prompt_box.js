47ff06f11f557d1b8018b4149f365677c606967c
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-08-23 16:55:40 +0530

    about-prompt-box

---
+++ Dialogue Boxes/prompt_box/prompt_box.js
@@ -0,0 +1,4 @@
+A prompt box is often used if you want the user to input a value before entering a page. 
+When a prompt box pops up, the user will have to click either "OK" or "Cancel" to proceed after entering an input value. 
+If the user clicks "OK" the box returns the input value.
+ If the user clicks "Cancel" the box returns null.
